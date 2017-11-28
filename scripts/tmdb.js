edf9cc0258af0c3e34b9eecde3e67a7a7dfeb2a1
Ryan Mcpherson <mmcpher2@gmail.com>
2017-11-28 15:53:54 -0600

    Added searched title and movie date to DOM

--- scripts/tmdb.js
+++ scripts/tmdb.js
@@ -5,11 +5,21 @@ const searchBar = function(event){
 
     $("#searchMovieInformation").on("click", event => {
         console.log("search Button is working")
+
         $.ajax({
             "url": `https://api.themoviedb.org/3/search/movie?api_key=3693ec3ce2f4a35cd73d00f01c34dcce&query=${$("#movieTitle").val()}`,
             "method": "GET"
-        }).then((results) => {
-        console.log(results)
+        }).then((searchedMovieData) => {
+            console.log(searchedMovieData)
+        let titleEl = $("#movieName")
+        titleEl.empty()
+        $("#movieTitle").val("")
+        searchedMovieData.results.forEach(function(movieObj) {
+            console.log(movieObj.title)
+            console.log(movieObj.release_date)
+            titleEl.append(`<p>${movieObj.title}</p>`)
+            titleEl.append(`<p>${movieObj.release_date}</p>`)
+        }, this);
         })
     })
 }

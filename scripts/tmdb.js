d07cdbe50e36d0cb036f155b476488dd0c77a3ba
Ryan Mcpherson <mmcpher2@gmail.com>
2017-11-29 08:45:48 -0600

    Added comments to code

--- scripts/tmdb.js
+++ scripts/tmdb.js
@@ -3,27 +3,34 @@ const $ = require("jquery")
 
 const searchBar = function(event){
 
+    // On "enter keypress" artificially click the searchMovieInfo id
     $("#movieTitle").on("keypress", function(event) {
         if (event.keyCode === 13) {
         $("#searchMovieInformation").click()}
     })
-
+    // On click of searchMovieInfo id, start ajax call
     $("#searchMovieInformation").on("click", event => {
         console.log("search Button is working")
-
+        // Get information from movie json based on search criteria in search field
         $.ajax({
             "url": `https://api.themoviedb.org/3/search/movie?api_key=3693ec3ce2f4a35cd73d00f01c34dcce&query=${$("#movieTitle").val()}`,
             "method": "GET"
         }).then((searchedMovieData) => {
             console.log(searchedMovieData)
-        let titleEl = $("#movieName")
-        titleEl.empty()
-        $("#movieTitle").val("")
-        searchedMovieData.results.forEach(function(movieObj) {
-            console.log(movieObj.title)
-            console.log(movieObj.release_date)
-            titleEl.append(`<p>${movieObj.title}</p>`)
-            titleEl.append(`<p>${movieObj.release_date}</p>`)
+            let titleEl = $("#movieName")
+
+            // Clear the search results contents
+            titleEl.empty()
+            // Clear the search criteria in input field
+            $("#movieTitle").val("")
+
+            // Pull the title, and release date of all search results and append them to DOM
+            searchedMovieData.results.forEach(function(movieObj) {
+                console.log(movieObj.title)
+                console.log(movieObj.release_date)
+                titleEl.append(`<img>${movieObj.poster_path}</img>`)
+                titleEl.append(`<p>${movieObj.title}</p>`)
+                titleEl.append(`<p>${movieObj.release_date}</p>`)
         }, this);
         })
     })

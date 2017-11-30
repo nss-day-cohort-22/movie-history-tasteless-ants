4bd06bfa4b178ada80dcce25e4c744e167277aec
Ryan Mcpherson <mmcpher2@gmail.com>
2017-11-30 11:29:35 -0600

    Showing search, unwatched, watched movies

--- scripts/watchlistController.js
+++ scripts/watchlistController.js
@@ -5,37 +5,44 @@ const firebase = require("firebase")
 const movieFactory = require("./movieFactory")
 const dataHandler = require("./dataHandler")
 
+let watchListDOM = function(event){
+
+    $("#your-movies").on("click", event => {
+
+        dataHandler.userTokenGET()
+            .then(watchList => {
+            // Clear the search results contents
 
 
-const watchlistDOM = function () {
-    $("#watchList").append(`
-    
-    `
-    )
-}
+            for (let key in watchList) {
 
+                if (watchList.hasOwnProperty(key)) {
 
+                        let watchListEl = $("#watchList")[0]
+                        let movieObj = watchList[key]
 
+                        let watchListDiv = document.createElement("div")
+                        watchListDiv.id = `movie_${movieObj.id}`
+                        watchListEl.append(watchListDiv)
 
+                        let movieImg = document.createElement("img")
+                        let movieTitle = document.createElement("p")
+                        let movieYear = document.createElement("p")
+                        let movieDeleteButton = document.createElement("button")
 
+                        movieImg.src = `https://image.tmdb.org/t/p/w500/${movieObj.poster_path}`
+                        movieTitle.appendChild(document.createTextNode(`${movieObj.title}`))
+                        movieYear.appendChild(document.createTextNode(`${movieObj.year}`))
+                        movieDeleteButton.classList.add(`deleteMovie_${movieObj.id}`)
+                        movieDeleteButton.appendChild(document.createTextNode("Delete from Watchlist"))
 
-
-
-
-
-
-
-// const showWatchlist = function(event){
-//     $("#unwatched-movies").on("click", event => {
-
-//         dataHandler.userTokenGET()
-//         .then(results => {
-//             console.log("======= hit")
-//             console.log(results)
-//         }).catch(err => {
-//             console.log("err" + err)
-//         })
-//     })
-// }
-
-// module.exports = showWatchlist
+                        watchListDiv.append(movieImg)
+                        watchListDiv.append(movieTitle)
+                        watchListDiv.append(movieYear)
+                        watchListDiv.append(movieDeleteButton)
+                    }
+                }
+            })
+        })
+    }
+    module.exports = watchListDOM

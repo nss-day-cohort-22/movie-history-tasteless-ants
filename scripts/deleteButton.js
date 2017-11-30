cc1677c5df3c493b47039f6e5516cd4e53099408
Courtney Seward <courtneyseward93@gmail.com>
2017-11-30 09:23:43 -0600

    new updates

---
+++ scripts/deleteButton.js
@@ -0,0 +1,15 @@
+const $ = require("jquery")
+const firebase = require("firebase")
+const auth = require("./auth/authorization")
+
+// click event to delete tracked movie when selected by user
+const DeletButton = function(event){
+    $("#deleteTrackedMovie").on(click, event)
+    // require data handler info
+    const dataHandler = require("./auth/dataHandler")
+{
+
+
+    }
+
+}
